1a014cd3bbcf59e8575d2473ee44f04afe55622d
topoika <topoikatampul@gmail.com>
2021-01-28 19:05:11 +0300

    new commit

--- lib/screens/splashscreen.dart
+++ lib/screens/splashscreen.dart
@@ -1,6 +1,7 @@
-import 'package:chowcub/screens/phoneverificationscreen.dart';
 import 'package:flutter/material.dart';
 import 'package:splash_screen_view/SplashScreenView.dart';
+
+import 'onboarding/screen_one.dart';
 
 class SplashScreen extends StatefulWidget {
   SplashScreen({Key key}) : super(key: key);
@@ -13,8 +14,8 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return SplashScreenView(
-      home: PhoneVerification(),
-      duration: 5000,
+      home: OnboardingScreenOne(),
+      duration: 8000,
       imageSize: 80,
       imageSrc: "assets/images/logo.png",
       text: "Chow Cub",
@@ -23,12 +24,26 @@ class _SplashScreenState extends State<SplashScreen> {
         fontSize: 40.0,
       ),
       colors: [
-        Colors.purple,
-        Colors.blue,
-        Colors.yellow,
+        Color(0xFFE00051),
+        Color(0xFF393B3F),
+        Color(0xFFFDD129),
         Colors.red,
       ],
       backgroundColor: Colors.white,
     );
+  }
+}
+
+class SplashSreen1 extends StatefulWidget {
+  SplashSreen1({Key key}) : super(key: key);
+
+  @override
+  _SplashSreen1State createState() => _SplashSreen1State();
+}
+
+class _SplashSreen1State extends State<SplashSreen1> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
