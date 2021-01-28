972b63d56cc72ce18c9015f087fd31abc20748ca
topoika <topoikatampul@gmail.com>
2021-01-28 17:58:07 +0300

    new commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chowcub/screens/splashscreen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,12 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Chow Cub',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: SplashScreen(),
     );
   }
 }
---
+++ lib/screens/splashscreen.dart
@@ -0,0 +1,34 @@
+import 'package:chowcub/screens/phoneverificationscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+
+class SplashScreen extends StatefulWidget {
+  SplashScreen({Key key}) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreenView(
+      home: PhoneVerification(),
+      duration: 5000,
+      imageSize: 100,
+      imageSrc: "assets/images/splash.png",
+      text: "Colorize Text",
+      textType: TextType.ColorizeAnimationText,
+      textStyle: TextStyle(
+        fontSize: 40.0,
+      ),
+      colors: [
+        Colors.purple,
+        Colors.blue,
+        Colors.yellow,
+        Colors.red,
+      ],
+      backgroundColor: Colors.white,
+    );
+  }
+}
