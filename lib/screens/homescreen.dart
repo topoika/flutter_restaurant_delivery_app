ab9bf1f74f54feeb4e05b8e9a7583fc55d2f7dfd
topoika <topoikatampul@gmail.com>
2021-02-04 11:11:36 +0300

    first commit

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/services.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -8,33 +10,170 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _value = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.symmetric(vertical: 40.0),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(vertical: 50.0),
         child: Column(
           children: [
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 9),
               height: 40,
-              decoration: BoxDecoration(color: Colors.blueGrey),
+              padding: EdgeInsets.symmetric(horizontal: 8),
+              decoration: BoxDecoration(
+                color: Colors.black12,
+              ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  Icon(Icons.settings),
-                  Row(
-                    children: [
-                      Text('Set an adress'),
-                      Icon(Icons.arrow_downward_outlined)
-                    ],
+                  //Icon(Icons.settings,color: Colors.white,),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.settings,
+                        color: Colors.white,
+                      )),
+                  SizedBox(
+                    width: 37,
                   ),
-                  Icon(Icons.restaurant_menu_rounded)
+                  Expanded(
+                      flex: 1,
+                      child: Row(
+                        children: [
+                          DropdownButton(
+                              value: _value,
+                              items: [
+                                DropdownMenuItem(
+                                  child: Text(
+                                    'Select An Address',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  value: 0,
+                                ),
+                                DropdownMenuItem(
+                                  child: Text("First Item"),
+                                  value: 1,
+                                ),
+                                DropdownMenuItem(
+                                  child: Text("Second Item"),
+                                  value: 2,
+                                ),
+                                DropdownMenuItem(
+                                    child: Text("Third Item"), value: 3),
+                                DropdownMenuItem(
+                                    child: Text("Fourth Item"), value: 4)
+                              ],
+                              icon: Icon(
+                                Icons.expand_more,
+                                color: Colors.white,
+                              ),
+                              onChanged: (value) {
+                                setState(() {
+                                  _value = value;
+                                });
+                              }),
+                        ],
+                      )),
+                  Expanded(
+                      flex: 1,
+                      child: IconButton(
+                          icon: Icon(
+                            Icons.menu,
+                            color: Colors.white,
+                          ),
+                          onPressed: () {}))
                 ],
               ),
             ),
             SizedBox(
               height: 10,
+            ),
+            Container(
+              child: SingleChildScrollView(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Card(
+                      elevation: 5.0,
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(color: Colors.red[400]),
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 8),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(50, 255, 255, 255),
+                  borderRadius: BorderRadius.all(Radius.circular(22.0)),
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Expanded(
+                        flex: 1,
+                        child: TextFormField(
+                          decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: 'Search for Foods or Restaurants..',
+                              hintStyle: TextStyle(color: Colors.white),
+                              icon: Icon(
+                                Icons.search,
+                                color: Colors.white,
+                              )),
+                        )),
+                    Expanded(
+                        flex: 0,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.mic, color: Colors.white),
+                                onPressed: () {}),
+                            VerticalDivider(
+                              color: Colors.white,
+                            ),
+                            IconButton(
+                                icon: Icon(
+                                  Icons.more_vert,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {})
+                          ],
+                        ))
+                  ],
+                ),
+              ),
+            ),
+            Divider(
+              height: 10,
+              thickness: 5,
+              color: Colors.black54,
+            ),
+            Container(
+              height: 40,
+              padding: EdgeInsets.symmetric(horizontal: 10, vertical: 8),
+              decoration: BoxDecoration(color: Colors.red[400]),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Expanded(flex: 1, child: Text('Top Restaurants')),
+                  Expanded(
+                      flex: 0,
+                      child: FlatButton(
+                        child: Text(
+                          'see all',
+                          style: TextStyle(color: Colors.white),
+                        ),
+                      ))
+                ],
+              ),
+            ),
+            Divider(
+              height: 20,
             ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 9),
